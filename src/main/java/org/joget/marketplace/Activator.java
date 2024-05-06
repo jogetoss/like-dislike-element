c4a3ae756a8a50f121ac493836ca996280e16269
AdyArsyad <ady.arsyad@joget.com>
2024-05-06 15:38:45 +0800

    Update version

--- src/main/java/org/joget/marketplace/Activator.java
+++ src/main/java/org/joget/marketplace/Activator.java
@@ -8,7 +8,7 @@ import org.osgi.framework.ServiceRegistration;
 
 public class Activator implements BundleActivator {
     
-    public static final String VERSION = "8.0.4";
+    public static final String VERSION = "8.0.5";
 
     protected Collection<ServiceRegistration> registrationList;
     
