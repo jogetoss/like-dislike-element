3e5e1ad104bfb3b9d3af0f0ab47b42ef969606d6
miaoxng <miaoxuan.ng@joget.com>
2024-05-08 23:56:56 +0800

    Modified: mandatory for redirect fields

--- src/main/java/org/joget/marketplace/Activator.java
+++ src/main/java/org/joget/marketplace/Activator.java
@@ -8,7 +8,7 @@ import org.osgi.framework.ServiceRegistration;
 
 public class Activator implements BundleActivator {
     
-    public static final String VERSION = "8.0.5";
+    public static final String VERSION = "8.0.6";
 
     protected Collection<ServiceRegistration> registrationList;
     
